7f172d8b4512afb0c8334d9e381bcb9f05b1f159
cathandstamp <cathandstamp@gmail.com>
2021-05-05 06:21:56 +0900

    chat AI

--- main.py
+++ main.py
@@ -66,10 +66,13 @@ def handle_message(event):
         event.reply_token,
 
         #TextSendMessage(text=event.message.text)) #ここでオウム返しのメッセージを返します。
-        words = (event.message.text)
-        response = client.talk(words)
-        TextSendMessage(("Bot >"+((response['results'])[0])['reply'])) #ここでオウム返しのメッセージを返します。
+          while words!="":
+              response = client.talk(words)
+              TextSendMessage("Bot >"+((response['results'])[0])['reply'])
+              words = event.message.text
     )
+
+
 
 
 # ポート番号の設定
